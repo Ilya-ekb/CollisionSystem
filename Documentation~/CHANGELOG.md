b53fe8853e5b3695a766342963d54d506cb5d860
ilyapolovinkin <2190055@gmail.com>
2025-09-08 18:33:07 +0300

    Trigger Enter 2D Processing

--- Documentation~/CHANGELOG.md
+++ Documentation~/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## [1.1.1] - 2025-09-08
+- Trigger Enter 2D Processing
+
 ## [1.1.0] - 2025-09-08
 - Trigger Enter Processing
 
--- Runtime/Core/CollisionReporter2D.cs
+++ Runtime/Core/CollisionReporter2D.cs
@@ -11,6 +11,15 @@ namespace CollisionSystem.Core
             if (CollisionManager.Default != null)
                 CollisionManager.Default.Register(gameObject, collision);
         }
+        
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(CollisionManager.Default != null)
+            {
+                var self = GetComponent<Collider2D>();
+                CollisionManager.Default.Register(gameObject, self, other);
+            }
+        }
     }
 }
 #endif
